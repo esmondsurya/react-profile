32919e640c2b573fa8b3a6d0cfd7385ffa969bb6
esmondsurya <69661292+esmondsurya@users.noreply.github.com>
2020-09-08 18:38:09 +0700

    added build

---
+++ react-portfolio-master/build/precache-manifest.1113b4a46dd614827daf1ddf556679fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0faeb78067c781098ceea377e6f4b395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123665d1bf492451f198",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123665d1bf492451f198",
+    "url": "/static/js/main.5df2a054.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
